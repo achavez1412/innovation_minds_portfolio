ccb1c34fb86a0385fc8c8226528bbb509c9e28a3
Alfredo Chavez <achavez0406@berkeley.edu>
2025-04-10 21:59:45 -0700

    completed: clean code for comments, links for icons, middleware comb., fixed/in prog: alignment of containers,some translation

--- root/server/middleware/language_middleware.js
+++ root/server/middleware/language_middleware.js
@@ -1,4 +1,5 @@
 const lang_validator=(req,res,next)=>{
+    //TODO:TRY/CATCH
     if(!req.query.key){
         return res.status(400).json({message: "Key Required"});
     } else if(typeof req.query.key !== "string"){
