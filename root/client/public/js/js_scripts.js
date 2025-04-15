1624f61759e04ab2e60384cdf15882ecf94c0c18
Alfredo Chavez <achavez0406@berkeley.edu>
2025-04-14 22:20:12 -0700

    alignment for laptop display, removed numbering, removed footer, added new font sizes/weights, started optimizing wrapper functions, new light mode, icons properly visualized

--- root/client/public/js/js_scripts.js
+++ root/client/public/js/js_scripts.js
@@ -1,39 +1,5 @@
-// var today = new Date();
-// document.getElementById("time_zone").innerHTML=today;
-
-//import the script from ajax_langauge
-//import { get_language_option } from "./ajax_language.js";
-
-//temp relative pathing, with imbedded json data
-//TODO:implement external json to fetch once web server started
-const json_temp = {
-    "introduction_blueprint": {
-        "en-us": "Welcome to Innovating Beyond the Blueprint!",
-        "es-mx": "¡Bienvenidos a Innovando Más Allá del Diseño!",
-        "zh-cn": "欢迎来到超越设计图的创新"
-    },
-    "introduction_self": {
-        "en-us": "My name is Alfredo Chavez, and I am an aspiring Electrical Engineer and Software Developer driven by a deep appreciation for resourcefulness, determination, and integrity.",
-        "es-mx": "Me llamo Alfredo Chávez y soy un aspirante de la Ingeniera Eléctrica y Computación, motivado por un aprecio fuerte del ingenio, la determinación y la integridad.",
-        "zh-cn": "我叫程惟恩。我是一名有抱负的电气和计算机工程师和我高度重视独创性、决心和正直。"
-    },
-    "network_connect" : {
-        "en-us": "If you're ever interested in connecting, I welcome and encourage the opportunity to exchange ideas and insights. I value meaningful conversation and the chance to learn even more from others, so feel free to reach out!",
-        "es-mx": "Si alguna vez te interesa conectar, te doy la bienvenida y favorezco la oportunidad de intercambiar ideas y perspectivas. Valoro las conversaciones significativas y la oportunidad de aprender aún más de los demás, ¡así que no dudes en contactarme!",
-        "zh-cn": "如果你有兴趣交流，我非常欢迎并鼓励这个机会来分享想法和见解。我珍惜有意义的对话，也期待向他人学习更多，随时欢迎联系！"
-    }
-}
-
-// var screen_velocity = .8;
-// function update(){
-//     var pos= window.screenTop;
-//     ".body_container".each(()=>{
-//         var element = this;
-//         var height = element.height - 20;
-//         this.css("backgroundPosition", "50% "+Math.round(height-pos)*screen_velocity+"px");
-//     });
-// };
-
+const regex_name_str = /[\p{L}]*/;
+const regex_email_str=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 
 //figure out if window load is necessary everywhere, how to restrict
@@ -41,20 +7,14 @@ window.onload=()=>{
     let dark_mode=localStorage.getItem('dark_mode');
     const input_theme = document.getElementById("dark_light");
     const body_main=document.querySelector(".body_main");
-    const lang_json = json_temp;
-    let prev_val = null;
-
+    
     const enableDarkMode=()=>{
         body_main.classList.remove("light_mode");
         body_main.classList.add("dark_mode");
         localStorage.setItem("dark_mode", "active");
-        //document.getElementsByClassName("body_image").style.setProperty("background_image", "url('../html/night_shadow_range.png')");
-        //console.log(document.getElementsByClassName("body_image").style.background_image, "COMPLETED");
     }
 
     const enableLightMode=()=>{
-        // document.body.querySelector(".body_main").classList.remove("dark_mode");
-        // document.body.querySelector(".body_main").classList.add("light_mode");
         body_main.classList.remove("dark_mode");
         body_main.classList.add("light_mode");
         localStorage.setItem("dark_mode", null);
@@ -63,9 +23,6 @@ window.onload=()=>{
     //implement UI behavior changes with focusing
     //if there is a change with the dark/light mode, enable the respective theme
     input_theme.addEventListener("change", (event)=>{
-        // document.getElementById("body_container").classList.toggle(prev_val);
-        // document.getElementById("body_container").classList.toggle(event.target.value);
-        //prev_val = event.target.value;
         dark_mode = localStorage.getItem("dark_mode");
         if(dark_mode !== "active"){
             enableDarkMode();
@@ -74,18 +31,6 @@ window.onload=()=>{
             enableLightMode();
         }
     });
-
-    //if there is a change of language selected, iterate through all text elements to selected lang
-    // document.getElementById("language_selector").addEventListener("change", (event)=>{
-    //     let json_key = event.target.value;
-    //     document.querySelectorAll(".translatable_text").forEach(element =>{
-    //         let element_id = element.id;
-    //         if(element_id === null || element_id === ''){
-    //             return;
-    //         }
-    //         element.textContent = lang_json[element_id][json_key];
-    //     });
-    // });
 
     //enable whatever mode is marked
     if(dark_mode === "active"){
@@ -122,26 +67,29 @@ $(document).ready(()=>{
         }
     }
 
-    //compress into 1 func
-    const name_checker=(name)=>{
-        if(name===null){
+    //returns bool for whether input is valid
+    const name_email_checker=(input_test,type='')=>{
+        if(input_test === null || typeof(input_test) !== "string" || type===''){
             return false;
         }
-        const regex_name_str = /[\p{L}]*/;
-        //const regex_name_str = /[A-Za-z]*/;
-        return regex_name_str.test(String(name));
+        let temp_regex_type;
+        if(type="name"){
+            temp_regex_type=regex_name_str;
+        } else if(type="email"){
+            temp_regex_type=regex_email_str;
+        }else{
+            return false;
+        }
+        console.log(`Returning Check of Type: ${type} and ${input_test} with Value: ${temp_regex_type.test(input_test)}`);
+        return temp_regex_type.test(String())
+    };
+
+    const name_checker=(name)=>{
+        return name_email_checker(name,"name");
     }
 
     const email_checker=(email)=>{
-        if(email===null){
-            return false;
-        }
-        console.log("email being checked");
-        //const regex_str=/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        //const regex_str=/^[A-Za-z0-9\.\-_]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
-        const regex_str=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        console.log(regex_str.test(email), " | ", email);
-        return regex_str.test(String(email));
+        return name_email_checker(email,"email");
     }
 
     //checks parameters(hard-coded)
@@ -331,3 +279,13 @@ $(document).ready(()=>{
 //};
 
 //consider enabling xmlhttpsrequest to dynamically translate webpage
+
+// var screen_velocity = .8;
+// function update(){
+//     var pos= window.screenTop;
+//     ".body_container".each(()=>{
+//         var element = this;
+//         var height = element.height - 20;
+//         this.css("backgroundPosition", "50% "+Math.round(height-pos)*screen_velocity+"px");
+//     });
+// };
